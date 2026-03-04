379f05e4bee766f135ab2eb653a46820a2f28015
INFRAGISTICS\IPetrov <IPetrov@infragistics.com>
2026-03-04 15:36:23 +0200

    fixed snippet names. added configs to webapp settings

--- MAKER.WebApp/Program.cs
+++ MAKER.WebApp/Program.cs
@@ -4,4 +4,20 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapGet("/api/config", (IConfiguration config) => new
+{
+    McpServerUrl = config["McpServerUrl"] ?? "http://localhost:5000",
+    Executor = new
+    {
+        Format = config["Executor:Format"] ?? "plaintext",
+        Clients = new
+        {
+            Planning        = new { Provider = config["Executor:Clients:Planning:Provider"],        Model = config["Executor:Clients:Planning:Model"] },
+            PlanVoting      = new { Provider = config["Executor:Clients:PlanVoting:Provider"],      Model = config["Executor:Clients:PlanVoting:Model"] },
+            Execution       = new { Provider = config["Executor:Clients:Execution:Provider"],       Model = config["Executor:Clients:Execution:Model"] },
+            ExecutionVoting = new { Provider = config["Executor:Clients:ExecutionVoting:Provider"], Model = config["Executor:Clients:ExecutionVoting:Model"] }
+        }
+    }
+});
+
 app.Run();
